4f6639f609a19454fd992aae354a338a5aabd5c8
ce1zer <jordylissenburg@gmail.com>
2025-11-14 21:38:23 +0100

    Remove search icon from database page search bar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -123,13 +123,12 @@ const Dashboard: React.FC = () => {
       {/* Search */}
       <div className="card-spotify">
         <div className="relative">
-          <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none z-10" />
           <input
             type="text"
             placeholder="Search by name or kennel..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="input-spotify w-full pl-14 pr-4"
+            className="input-spotify w-full"
           />
         </div>
       </div>
